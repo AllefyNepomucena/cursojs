a0706ebeccae09a9d58630608bc50ade9d297498
AllefyNepomucena <allefy.s.nepomucena@gmail.com>
2023-11-19 22:09:28 -0300

    aula 12

---
+++ aula12/ex014.js
@@ -0,0 +1,45 @@
+var agora = new Date()
+var diasem = agora.getDay()
+
+
+
+
+switch(diasem) {
+    case 0 :
+        console.log('Domingo')
+        break
+
+     case 1 :
+            console.log('Segunda')
+            break
+
+        case 2 :
+            console.log('Terça')
+            break
+
+            case 3 : 
+                console.log('Quartou')   
+                break
+
+                case 4 :
+                    console.log('Quinta')
+                    break
+
+                    case 5 :
+                        console.log('SEXTOOU')
+                        break
+
+                        case 6 :
+                            console.log('Sabadou ')
+                            break
+
+                            case 7 :
+                                console.log('Domingou')
+                                break
+
+                            default:
+                                console.log('Data Invalida')
+
+
+            
+}
