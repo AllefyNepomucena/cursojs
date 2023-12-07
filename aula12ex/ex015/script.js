4b9f467fbedf6a0a9867c1c919413d8a41c8eab1
AllefyNepomucena <allefy.s.nepomucena@gmail.com>
2023-12-06 21:32:15 -0300

    .

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,10 +3,24 @@ function verificar(){
    var ano = data.getFullYear()
    var fano = document.getElementById('txtano')
    var res = document.querySelector('div#res')
-   if(fano.value.length == 0 || fano.value > ano){
+   if(fano.value.length == 0 || Number(fano.value) > ano){
      window.alert('[ERRO] Verifique Seus Dados')
    }
    else{
-    window.alert('tudo ok')
+     var fsex = document.getElementsByName('radsex')
+     var idade = ano - Number(fano.value)
+     var genero = ''
+     if (fsex[0].checked){
+      genero = 'Homem'
+      if (idade >=0 && < 10){
+        criança
+      }
+      else if (idade < 21)
+      }
+     }
+     else if(fsex[1].checked)
+     genero = " mulher"
    }
+  res.innerHTML= `Detequitamos ${genero} de ${idade} anos.`
+  res.style.textAlingn = 'center'
 }
