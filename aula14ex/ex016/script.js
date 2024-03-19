c84b565c47e7e1038b9ed900d3b8eb9aeda9f111
AllefyNepomucena <allefy.s.nepomucena@gmail.com>
2024-03-19 16:21:20 -0300

    ......

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,17 @@
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+
+    if(ini.value.length == 0 || fim.value.leght == 0 || passo.value.length == 0 ){
+        window.alert('[ERRO] : Faltou Dados')
+    }
+    else{
+       res.innerHTML = 'Contando...'
+       let i = Number(inicio.value)
+       let f =Number(fim.value)
+       let p =Number(passo.value)
+
+       for()
+    }
+}
