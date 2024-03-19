1240b45904264f89ee1302cf9ae21eac75be5d89
AllefyNepomucena <allefy.s.nepomucena@gmail.com>
2024-03-19 18:06:58 -0300

    ex16 feito

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -2,16 +2,33 @@ function contar() {
     var ini = document.getElementById('txti')
     var fim = document.getElementById('txtf')
     var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
 
     if(ini.value.length == 0 || fim.value.leght == 0 || passo.value.length == 0 ){
-        window.alert('[ERRO] : Faltou Dados')
+        res.innerHTML = 'impossivel contar!'
+        //window.alert('[ERRO] : Faltou Dados')
     }
     else{
-       res.innerHTML = 'Contando...'
-       let i = Number(inicio.value)
+       res.innerHTML = 'Contando :'
+       let i = Number(ini.value)
        let f =Number(fim.value)
        let p =Number(passo.value)
+       if(p<=0){
+        window.alert('passo invalido, considera a partir de passo 1')
+        p = 1
+       }
+        if( i < f ){
+            for(let c = i ; c<=f ; c+= p){
+                res.innerHTML += `  ${c} \u{1f449}`
+              }
+               
+        }else{
 
-       for()
+            for(let c = i ; c >=f; c-=p){
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+            res.innerHTML +=`\u{1f3c1}`
+        }
+      
     }
 }
