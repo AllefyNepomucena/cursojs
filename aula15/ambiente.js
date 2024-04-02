20685f02fda8d77e70355351c4d863ddcbc31638
AllefyNepomucena <allefy.s.nepomucena@gmail.com>
2024-04-02 19:12:21 -0300

    Finalizado a aula 15

---
+++ aula15/ambiente.js
@@ -0,0 +1,13 @@
+let num =[5,8,2,9,3]
+num.sort()
+num[5]=10
+num.push(11)
+console.log(`Nosso vetor primeiro vetor e ${num[0]}`)
+console.log(`o vetor tem ${num.length} posiçoes`)
+
+let pos = num.indexOf(8)
+if (pos == -1 ){
+    console.log(`o valor não foi encontrado`)
+}else{
+    console.log(`ò valor esta na posicao ${pos}`)
+}
